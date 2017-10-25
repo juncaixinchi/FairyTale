f892f6c479258a3876f1de96df69aae1d710107a
juncaixinchi <juncaixinchi111@gmail.com>
2017-10-25 12:30:39 +0000

    add log

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const path = require('path')
 const express = require('express')
+const morgan = require('morgan')
 
 const port = 80 // need sudo
 const app = express()
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
   res.send('Hello world !')
 })
 
 app.use('/fgo', express.static(path.join(__dirname, 'public/FGO'), { index: 'fgo.html' }))
-app.use('/Blogs', express.static(path.join(__dirname, 'public/Blogs')))
+// app.use('/Blogs', express.static(path.join(__dirname, 'public/Blogs')))
 
 const server = app.listen(port, () => {
   console.log(`服务已启动 http://localhost:${port}`)
