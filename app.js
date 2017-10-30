8ec13ab27efc8d58ddda244dc8c8faa834b138bd
juncaixinchi <juncaixinchi111@gmail.com>
2017-10-30 19:58:32 +0800

    forward google

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const request = require('request')
 const morgan = require('morgan')
 
 const port = 80 // need sudo
@@ -12,6 +13,11 @@ app.get('/', (req, res) => {
 
 app.use('/fgo', express.static(path.join(__dirname, 'public/FGO'), { index: 'fgo.html' }))
 // app.use('/Blogs', express.static(path.join(__dirname, 'public/Blogs')))
+
+app.use('/google', (req, res) => {
+  const url = 'https://www.google.com/' + req.url
+  req.pipe(request(url)).pipe(res)
+})
 
 const server = app.listen(port, () => {
   console.log(`服务已启动 http://localhost:${port}`)
