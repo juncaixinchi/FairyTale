f0a06c9a6fa79e4cc01aeb4b4449f653fedf3e0a
juncaixinchi <juncaixinchi111@gmail.com>
2017-10-31 01:34:19 +0000

    fix bug

--- app.js
+++ app.js
@@ -7,18 +7,16 @@ const port = 80 // need sudo
 const app = express()
 app.use(morgan('combined'))
 
-app.get('/', (req, res) => {
-  res.send('Hello world !')
+// app.get('/', (req, res) => res.send('Hello world !'))
+
+app.use('/', (req, res) => {
+  const url = 'https://www.google.com/' + req.url
+  req.pipe(request(url)).pipe(res)
 })
 
 app.use('/fgo', express.static(path.join(__dirname, 'public/FGO'), { index: 'fgo.html' }))
 // app.use('/Blogs', express.static(path.join(__dirname, 'public/Blogs')))
 
-app.use('/google', (req, res) => {
-  const url = 'https://www.google.com/' + req.url
-  req.pipe(request(url)).pipe(res)
-})
-
 const server = app.listen(port, () => {
-  console.log(`服务已启动 http://localhost:${port}`)
+  console.log(`server start on http://localhost:${port}`)
 })
